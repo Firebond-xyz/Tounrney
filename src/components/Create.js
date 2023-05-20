1b94c332d3572d2914ca2238b2db713503ceb7ac
iam-sphinx <kandarp1803@gmail.com>
2023-05-20 16:03:56 +0530

    feat: CSS

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 const Create = () => {
-  const [tournamentName, setTournamentName] = useState('');
-  const [tournamentType, setTournamentType] = useState('');
-  const [playerIds, setPlayerIds] = useState('');
-  const [participantCount, setParticipantCount] = useState('');
-  const [gameName, setGameName] = useState('');
+  const [tournamentName, setTournamentName] = useState("");
+  const [tournamentType, setTournamentType] = useState("");
+  const [playerIds, setPlayerIds] = useState("");
+  const [participantCount, setParticipantCount] = useState("");
+  const [gameName, setGameName] = useState("");
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
@@ -22,61 +22,181 @@ const Create = () => {
   };
 
   return (
-    <form onSubmit={handleFormSubmit}>
-      <div>
-        <label htmlFor="tournamentName">Tournament Name:</label>
-        <input
-          type="text"
-          id="tournamentName"
-          value={tournamentName}
-          onChange={(e) => setTournamentName(e.target.value)}
-        />
+    <div className="min-h-screen bg-gradient-to-br from-[#141e30] to-[#243b55]">
+      <h1 className="font-bold font-signature text-center text-5xl p-8 text-white mb-12">
+        Tournament Bracket Generator
+      </h1>
+      <div className="w-full flex justify-center items-center">
+        <form className="" onSubmit={handleFormSubmit}>
+          {/* Tournament name */}
+          <div className="flex w-[625px] gap-6 justify-between items-center mb-6">
+            <label
+              className="w-full text-end text-[#A9A9A9] text-lg font-medium "
+              htmlFor="tournamentName"
+            >
+              Tournament Name:{" "}
+            </label>
+            <div className="w-full h-[40px] rounded-lg overflow-hidden text-ellipsis bg-[#303540] hover:border hover:border-amber-600">
+              <input
+                className="w-full h-full rounded-lg bg-inherit px-6 overflow-hidden text-ellipsis outline-amber-600 text-white font-base font-normal"
+                type="text"
+                id="tournamentName"
+                value={tournamentName}
+                onChange={(e) => {
+                  setTournamentName(e.target.value);
+                }}
+              />
+            </div>
+          </div>
+
+          {/* Tournament Type */}
+          <div className="flex w-[625px] gap-6 justify-between items-center mb-6">
+            <label
+              className="text-end w-full text-[#A9A9A9] text-lg font-medium "
+              htmlFor="tournamentType"
+            >
+              Tournament Type:{" "}
+            </label>
+            <select
+              className="w-full h-[40px] rounded-md text-[#A9A9A9] text-lg font-normal "
+              id="tournamentType"
+              value={tournamentType}
+              onChange={(e) => setTournamentType(e.target.value)}
+            >
+              <option value="">Select Type</option>
+              <option value="singleElimination">Single Elimination</option>
+              <option value="doubleElimination">Double Elimination</option>
+            </select>
+          </div>
+
+          {/* Player Ids */}
+          <div className="flex w-[625px] gap-6  justify-between items-center mb-6">
+            <label
+              className="text-end w-full text-[#A9A9A9] text-lg font-medium "
+              htmlFor="playerIds"
+            >
+              Player IDs:{" "}
+            </label>
+            <div className="w-full h-[40px] rounded-lg overflow-hidden text-ellipsis bg-[#303540] hover:border hover:border-amber-600">
+              <input
+                className="w-full h-full rounded-lg bg-inherit px-6 overflow-hidden text-ellipsis outline-amber-600 text-white font-base font-normal"
+                type="text"
+                id="playerIds"
+                value={playerIds}
+                onChange={(e) => setPlayerIds(e.target.value)}
+              />
+            </div>
+          </div>
+
+          {/* Participant Count */}
+          <div className="flex w-[625px] gap-6 justify-between items-center mb-6">
+            <label
+              className="w-full text-end text-[#A9A9A9] text-lg font-medium "
+              htmlFor="participantCount"
+            >
+              Number of Participants:{" "}
+            </label>
+            <div className="w-full h-[40px] rounded-lg overflow-hidden text-ellipsis bg-[#303540] hover:border hover:border-amber-600">
+              <input
+                className="w-full h-full rounded-lg bg-inherit px-6 overflow-hidden text-ellipsis outline-amber-600 text-white font-base font-normal"
+                type="number"
+                id="participantCount"
+                value={participantCount}
+                onChange={(e) => setParticipantCount(e.target.value)}
+              />
+            </div>
+          </div>
+
+          {/* Game Name */}
+          <div className="flex w-[625px] gap-6 justify-between items-center mb-6">
+            <label
+              className="w-full text-end text-[#A9A9A9] text-lg font-medium "
+              htmlFor="gameName"
+            >
+              Game Name:{" "}
+            </label>
+            <div className="w-full h-[40px] rounded-lg overflow-hidden text-ellipsis bg-[#303540] hover:border hover:border-amber-600">
+              <input
+                className="w-full h-full rounded-lg bg-inherit px-6 overflow-hidden text-ellipsis outline-amber-600 text-white font-base font-normal"
+                type="text"
+                id="gameName"
+                value={gameName}
+                onChange={(e) => setGameName(e.target.value)}
+              />
+            </div>
+          </div>
+
+          {/* Link to brackets */}
+          <div className="flex w-[625px] gap-6 justify-between items-center mb-6">
+            <div className="w-full"></div>
+            <div className="w-full flex ">
+              <Link
+                to="/brackets"
+                className="w-auto text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 "
+              >
+                <button type="submit">Submit</button>
+              </Link>
+            </div>
+          </div>
+        </form>
       </div>
-      <div>
-        <label htmlFor="tournamentType">Tournament Type:</label>
-        <select
-          id="tournamentType"
-          value={tournamentType}
-          onChange={(e) => setTournamentType(e.target.value)}
-        >
-          <option value="">Select Type</option>
-          <option value="singleElimination">Single Elimination</option>
-          <option value="doubleElimination">Double Elimination</option>
-        </select>
-      </div>
-      <div>
-        <label htmlFor="playerIds">Player IDs:</label>
-        <input
-          type="text"
-          id="playerIds"
-          value={playerIds}
-          onChange={(e) => setPlayerIds(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="participantCount">Number of Participants:</label>
-        <input
-          type="number"
-          id="participantCount"
-          value={participantCount}
-          onChange={(e) => setParticipantCount(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="gameName">Game Name:</label>
-        <input
-          type="text"
-          id="gameName"
-          value={gameName}
-          onChange={(e) => setGameName(e.target.value)}
-        />
-      </div>
-      <Link to='/brackets'>
-      <button type="submit">Submit</button>
-      
-      </Link>
-    </form>
+    </div>
+
+    // <form onSubmit={handleFormSubmit} className='min-h-screen bg-[#272a33]'>
+    //   <div>
+    //     <label htmlFor="tournamentName">Tournament Name:</label>
+    //     <input
+    //       type="text"
+    //       id="tournamentName"
+    //       value={tournamentName}
+    //       onChange={(e) => setTournamentName(e.target.value)}
+    //     />
+    //   </div>
+    //   <div>
+    //     <label htmlFor="tournamentType">Tournament Type:</label>
+    //     <select
+    //       id="tournamentType"
+    //       value={tournamentType}
+    //       onChange={(e) => setTournamentType(e.target.value)}
+    //     >
+    //       <option value="">Select Type</option>
+    //       <option value="singleElimination">Single Elimination</option>
+    //       <option value="doubleElimination">Double Elimination</option>
+    //     </select>
+    //   </div>
+    //   <div>
+    //     <label htmlFor="playerIds">Player IDs:</label>
+    //     <input
+    //       type="text"
+    //       id="playerIds"
+    //       value={playerIds}
+    //       onChange={(e) => setPlayerIds(e.target.value)}
+    //     />
+    //   </div>
+    //   <div>
+    //     <label htmlFor="participantCount">Number of Participants:</label>
+    //     <input
+    //       type="number"
+    //       id="participantCount"
+    //       value={participantCount}
+    //       onChange={(e) => setParticipantCount(e.target.value)}
+    //     />
+    //   </div>
+    //   <div>
+    //     <label htmlFor="gameName">Game Name:</label>
+    //     <input
+    //       type="text"
+    //       id="gameName"
+    //       value={gameName}
+    //       onChange={(e) => setGameName(e.target.value)}
+    //     />
+    //   </div>
+    //   <Link to='/brackets'>
+    //   <button type="submit">Submit</button>
+
+    //   </Link>
+    // </form>
   );
 };
 
-export default Create 
+export default Create;
--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -1,6 +1,7 @@
 import { isLabelWithInternallyDisabledControl } from "@testing-library/user-event/dist/utils";
 import { ethers } from "ethers";
 import React, { useState } from "react";
+import MetamaskBtn from "./MetamaskBtn";
 
 const Walletcaed = () => {
   const [errorMessage, setErrorMessage] = useState(null);
@@ -22,29 +23,43 @@ const Walletcaed = () => {
     getAccountBalance(newAccount);
   };
   const getAccountBalance = (account) => {
-    window.ethereum.request({
-      method: "eth_getBalance",
-      params: [account, "latest"],
-    }).then(balance => (setUserBalance(ethers.utils.formatEther(balance) )));
+    window.ethereum
+      .request({
+        method: "eth_getBalance",
+        params: [account, "latest"],
+      })
+      .then((balance) => setUserBalance(ethers.utils.formatEther(balance)));
   };
-  const chainChangedHandler = () =>{
-    window.location.reload()
-  }
+  const chainChangedHandler = () => {
+    window.location.reload();
+  };
   window.ethereum.on("accountChanged", accountChangeHandler);
 
-  window.ethereum.on("chainChanged",chainChangedHandler)
-
+  window.ethereum.on("chainChanged", chainChangedHandler);
 
   return (
-    <div className="Walletcard">
-      <h4>{"Connect to Metamask using windows.ethereum method"}</h4>
-      <button onClick={connectWalletHandler}>{connButtonText}</button>
-      <div className="accountDisplay">
-        <h3>Accounts: {defaultAccount}</h3>
+    <div className="Walletcard ">
+      <div className="w-full h-20 border-b border-[#353B43] flex justify-between items-center px-6 overflow-hidden text-ellipsis">
+        <h4 className="font-semibold text-center p-6 text-[#A9A9A9] text-3xl font-signature">
+          {"Connect to Metamask using windows.ethereum method"}
+        </h4>
+        <MetamaskBtn
+          onClick={connectWalletHandler}
+          title={connButtonText}
+        ></MetamaskBtn>
       </div>
-      <div className="balanceDisplay">
-        <h3>Balance: {userBalance}</h3>
+
+      <div className=" mt-40 mb-20 flex justify-center items-center">
+        <div>
+          <div className="accountDisplay text-slate-400 font-bold text-3xl font-title mb-6 ">
+            <h3>Accounts: {defaultAccount}</h3>
+          </div>
+          <div className="balanceDisplay text-slate-400 font-bold text-3xl font-title">
+            <h3>Balance: {userBalance}</h3>
+          </div>
+        </div>
       </div>
+
       {errorMessage}
     </div>
   );
