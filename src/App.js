0a3bbbfe2d41a4a80588fa2a7b2fdc12f01a85d3
Ameya <adengane@gmai.com>
2023-05-20 13:32:07 +0530

    created a network switch function

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import Create from './components/Create';
 import Join from './components/Join';
 import Brackets from '../src/components/Brackets';
 import { Routes, Route, Switch } from 'react-router-dom';
-import { ethers } from "ethers";
 import Counter from "../src/abi/bracket.json";
-import Wallet from './components/Wallet'
 
 const counterAddress = "0xdF3B919239A65047A1C0eCF40D8e8C9621A94459";
 
--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Link, useNavigate } from "react-router-dom";
 import Wallet from '../../components/Wallet'
+import Switch from '../../components/Switch'
 const Main = () => {
 
   
   return (
     <div className=''>
       <Wallet />
+      <Switch />
       <div className=''>
 
 
